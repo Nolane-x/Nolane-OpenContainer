d3c5ec641533786f5621b3493e690c7f69821498
Nolane-x <casioreview20@gmail.com>
2026-09-26 11:05:51 +0700

    feat: expose OPFS workspace persistence in SDK

--- packages/sdk/src/index.js
+++ packages/sdk/src/index.js
@@ -1,6 +1,7 @@
+import { ErrorCodes, assertOc } from '../../protocol/src/index.js';
 import { DiagnosticJournal } from '../../diagnostics/src/index.js';
 import { ResourceGovernor } from '../../resources/src/index.js';
-import { MemoryVFS } from '../../vfs/src/index.js';
+import { MemoryVFS, OpfsCheckpointAuthority } from '../../vfs/src/index.js';
 import { ProcessSupervisor } from '../../process/src/index.js';
 import { PackageGraphAuthority } from '../../package-env/src/index.js';
 import { NetworkAuthority } from '../../network/src/index.js';
@@ -19,11 +20,20 @@ export class OpenContainer {
     const preview=new PreviewAuthority();
     const snapshots=new MemoryPersistenceAuthority({fs});
     const kernel=new OpenContainerKernel({diagnostics});
-    Object.assign(this,{fs,process,packages,net,preview,snapshots,resources,diagnostics});
+    Object.assign(this,{fs,process,packages,net,preview,snapshots,resources,diagnostics,workspacePersistence:null});
     Object.defineProperty(this,'_kernel',{value:kernel,enumerable:false});
+    Object.defineProperty(this,'_workspacePersistenceOptions',{value:options.workspacePersistence??null,enumerable:false});
   }
   static async boot(options={}){const runtime=new OpenContainer(options);await runtime.boot();return runtime;}
-  async boot(){await this._kernel.boot();return this;}
+  async boot(){
+    if(this._workspacePersistenceOptions){
+      const authority=await new OpfsCheckpointAuthority(this._workspacePersistenceOptions).open();
+      await authority.restoreInto(this.fs);
+      this.workspacePersistence=authority;
+    }
+    await this._kernel.boot();
+    return this;
+  }
   get state(){return this._kernel.state;}
   get health(){return this._kernel.health;}
   mount(files){this._kernel.assertReady();return this.fs.mount(files);}
@@ -31,6 +41,16 @@ export class OpenContainer {
   spawn(command,args=[],options={}){this._kernel.assertReady();return this.process.spawn(command,args,options);}
   listen(port,handler,{owner='runtime'}={}){this._kernel.assertReady();return this.preview.publish({port,owner,handler});}
   installPackageCommands(options={}){this._kernel.assertReady();return this.packages.bindCommands(this.process,options);}
+  async persistWorkspace(){
+    this._kernel.assertReady();
+    assertOc(this.workspacePersistence,ErrorCodes.INVALID_STATE,'Workspace OPFS persistence is not configured');
+    return this.workspacePersistence.checkpoint(this.fs);
+  }
+  async collectWorkspaceGarbage(options={}){
+    this._kernel.assertReady();
+    assertOc(this.workspacePersistence,ErrorCodes.INVALID_STATE,'Workspace OPFS persistence is not configured');
+    return this.workspacePersistence.collectGarbage(options);
+  }
   async terminate(){await this._kernel.terminate();}
 }
 
